88e38b0e8f2b9b5ed7189410bf420a6c4cf454e5
sanket joshi <Sanket.joshi086@gmail.com>
2017-02-16 15:56:00 +0530

    Add files via upload
    Render added

---
+++ PopupNavigationIOS.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using Foundation;
+using UIKit;
+using ValuD.MobileApp.IWSM.Popup.IOS.Impl;
+using Xamarin.Forms.Platform.iOS;
+using ValuD.MobileApp.IWSM.iOS.Common;
+using Xamarin.Forms;
+using Popup;
+
+[assembly: Dependency(typeof(PopupNavigationIOS))]
+namespace ValuD.MobileApp.IWSM.Popup.IOS.Impl
+{
+    [Preserve(AllMembers = true)]
+    internal class PopupNavigationIOS : IPopupNavigation
+    {
+        public void AddPopup(PopupPage page)
+        {
+            var topViewController = GetTopViewController();
+            var topRenderer = topViewController.ChildViewControllers.LastOrDefault() as IVisualElementRenderer;
+
+            if (topRenderer != null)
+                page.Parent = topRenderer.Element;
+            else
+                page.Parent = Application.Current.MainPage;
+
+            var renderer = page.GetOrCreateRenderer();
+
+            topViewController.View.AddSubview(renderer.NativeView);
+        }
+
+        public void RemovePopup(PopupPage page)
+        {
+            var renderer = page.GetOrCreateRenderer();
+            var viewController = renderer?.ViewController;
+
+            if (viewController != null && !viewController.IsBeingDismissed)
+                renderer.NativeView.RemoveFromSuperview();
+
+        }
+
+        private UIViewController GetTopViewController()
+        {
+            var topViewController = UIApplication.SharedApplication.KeyWindow.RootViewController;
+
+            while (topViewController.PresentedViewController != null)
+            {
+                topViewController = topViewController.PresentedViewController;
+            }
+
+            return topViewController;
+        }
+    }
+}
---
+++ PopupPageRenderer.cs
@@ -0,0 +1,107 @@
+﻿using System.Linq;
+using Foundation;
+using ValuD.MobileApp.IWSM.Popup.IOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using Size = Xamarin.Forms.Size;
+using Popup;
+
+[assembly: ExportRenderer(typeof(PopupPage), typeof(PopupPageRenderer))]
+namespace ValuD.MobileApp.IWSM.Popup.IOS.Renderers
+{
+    [Preserve(AllMembers = true)]
+    public class PopupPageRenderer : PageRenderer
+    {
+
+        private PopupPage _element
+        {
+            get { return (PopupPage) Element; }
+        }
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+
+            var tapGesture = new UITapGestureRecognizer(OnTap)
+            {
+                CancelsTouchesInView = false
+            };
+
+            if (e.NewElement != null)
+            {
+                ModalPresentationStyle = UIModalPresentationStyle.OverCurrentContext;
+                ModalTransitionStyle = UIModalTransitionStyle.CoverVertical;
+
+                View.AddGestureRecognizer(tapGesture);
+            }
+            if (e.OldElement != null)
+            {
+                View.RemoveGestureRecognizer(tapGesture);
+            }           
+        }
+
+        public override void ViewDidAppear(bool animated)
+        {
+            base.ViewDidAppear(animated); 
+        }
+
+        private void OnTap(UITapGestureRecognizer e)
+        {
+            var view = e.View;
+            var location = e.LocationInView(view);
+            var subview = view.HitTest(location, null);
+            if (subview == view)
+            {
+                _element.SendBackgroundClick();
+            }
+        }
+
+        public override void ViewDidLayoutSubviews()
+        {
+            base.ViewDidLayoutSubviews();
+            SetElementSize(new Size(View.Bounds.Width, View.Bounds.Height));
+        }
+
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+            NSNotificationCenter.DefaultCenter.AddObserver(UIKeyboard.WillChangeFrameNotification, KeyBoardUpNotification);
+            NSNotificationCenter.DefaultCenter.AddObserver(UIKeyboard.WillHideNotification,KeyBoardDownNotification);
+        }
+
+        public override void ViewWillDisappear(bool animated)
+        {
+            base.ViewWillDisappear(animated);
+            NSNotificationCenter.DefaultCenter.RemoveObserver(this);
+
+            var isNotRemoved = PopupNavigation.PopupStack.Any(e => e == _element);
+
+            // Close all open pages the Popup, if the main page, on which opened PresentViewControllerAsync, destroyed.
+            if (isNotRemoved)
+            {
+                RemoveThisPageFromStack();
+            }
+        }
+
+        private void KeyBoardUpNotification(NSNotification notifi)
+        {
+            var r = UIKeyboard.BoundsFromNotification(notifi);
+            var screen = UIScreen.MainScreen.Bounds;
+
+            SetElementSize(new Size(r.Width, screen.Height - r.Height));
+        }
+
+        private void KeyBoardDownNotification(NSNotification notifi)
+        {
+            var r = UIKeyboard.BoundsFromNotification(notifi);
+            var screen = UIScreen.MainScreen.Bounds;
+
+            SetElementSize(new Size(r.Width, screen.Height));
+        }
+
+        private async void RemoveThisPageFromStack()
+        {
+            await PopupNavigation.RemovePageAsync(_element, false);
+        }
+    }
+}
